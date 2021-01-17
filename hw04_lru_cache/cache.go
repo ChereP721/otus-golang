f979e2b4179d9edea0cef50c60336783428bac91
cherep721 <alexnorin721@gmail.com>
2021-01-17 20:03:20 +0300

    HW #4 version 1

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,22 +1,91 @@
 package hw04_lru_cache //nolint:golint,stylecheck
+import "sync"
 
 type Key string
 
 type Cache interface {
-	// Place your code here
+	Set(key Key, value interface{}) bool
+	Get(key Key) (interface{}, bool)
+	Clear()
 }
 
 type lruCache struct {
-	// Place your code here:
-	// - capacity
-	// - queue
-	// - items
+	mu       sync.Mutex
+	capacity int
+	queue    List
+	items    map[Key]*listItem
 }
 
 type cacheItem struct {
-	// Place your code here
+	key   Key
+	value interface{}
+}
+
+func getCacheItemPtr(value interface{}) *cacheItem {
+	ciPtr, ok := value.(*cacheItem)
+	if !ok {
+		// поскольку в интерфейсе нет возврата ошибки - паникуем
+		panic("Wrong cache item type!")
+	}
+
+	return ciPtr
+}
+
+func (cache *lruCache) Set(key Key, value interface{}) bool {
+	cache.mu.Lock()
+	defer cache.mu.Unlock()
+
+	li, ok := cache.items[key]
+	if ok {
+		getCacheItemPtr(li.Value).value = value
+		cache.queue.MoveToFront(li)
+
+		return true
+	}
+
+	if len(cache.items) == cache.capacity {
+		delKey := getCacheItemPtr(cache.queue.Back().Value).key
+		cache.queue.Remove(cache.queue.Back())
+		delete(cache.items, delKey)
+	}
+
+	newItem := cache.queue.PushFront(&cacheItem{
+		key:   key,
+		value: value,
+	})
+	cache.items[key] = newItem
+
+	return false
+}
+
+func (cache *lruCache) Get(key Key) (interface{}, bool) {
+	cache.mu.Lock()
+	defer cache.mu.Unlock()
+
+	li, ok := cache.items[key]
+	if !ok {
+		return nil, false
+	}
+
+	cache.queue.MoveToFront(li)
+	return getCacheItemPtr(li.Value).value, true
+}
+
+func (cache *lruCache) Clear() {
+	cache.mu.Lock()
+	defer cache.mu.Unlock()
+
+	for k, li := range cache.items {
+		cache.queue.Remove(li)
+		delete(cache.items, k)
+	}
 }
 
 func NewCache(capacity int) Cache {
-	return &lruCache{}
+	return &lruCache{
+		mu:       sync.Mutex{},
+		capacity: capacity,
+		queue:    NewList(),
+		items:    make(map[Key]*listItem),
+	}
 }
--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -1,15 +1,100 @@
 package hw04_lru_cache //nolint:golint,stylecheck
 
 type List interface {
-	// Place your code here
+	Len() int                          // длина списка
+	Front() *listItem                  // первый элемент списка
+	Back() *listItem                   // последний элемент списка
+	PushFront(v interface{}) *listItem // добавить значение в начало
+	PushBack(v interface{}) *listItem  // добавить значение в конец
+	Remove(i *listItem)                // удалить элемент
+	MoveToFront(i *listItem)           // переместить элемент в начало
 }
 
 type listItem struct {
-	// Place your code here
+	Value interface{} // значение
+	Next  *listItem   // следующий элемент
+	Prev  *listItem   // предыдущий элемент
 }
 
 type list struct {
-	// Place your code here
+	frontItem *listItem
+	backItem  *listItem
+	len       int
+}
+
+func (l list) Len() int {
+	return l.len
+}
+
+func (l list) Front() *listItem {
+	return l.frontItem
+}
+
+func (l list) Back() *listItem {
+	return l.backItem
+}
+
+func (l *list) PushFront(v interface{}) *listItem {
+	li := listItem{
+		v,
+		l.frontItem,
+		nil,
+	}
+
+	if l.frontItem == nil {
+		l.backItem = &li
+	} else {
+		l.frontItem.Prev = &li
+	}
+
+	l.frontItem = &li
+	l.len++
+
+	return &li
+}
+
+func (l *list) PushBack(v interface{}) *listItem {
+	li := listItem{
+		v,
+		nil,
+		l.backItem,
+	}
+
+	if l.backItem != nil {
+		l.backItem.Next = &li
+	} else {
+		l.frontItem = &li
+	}
+
+	l.backItem = &li
+	l.len++
+
+	return &li
+}
+
+func (l *list) Remove(i *listItem) {
+	// неплохо бы проверить, что элемент является членом этого списка, т.к. в задании сказано, что "Гарантируется, что методы Remove и MoveToFront вызываются от существующих в списке элементов." - пропускаем
+	if i.Prev != nil {
+		i.Prev.Next = i.Next
+	} else {
+		l.frontItem = i.Next
+	}
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	} else {
+		l.backItem = i.Prev
+	}
+
+	l.len--
+}
+
+func (l *list) MoveToFront(i *listItem) {
+	// неплохо бы проверить, что элемент является членом этого списка, т.к. в задании сказано, что "Гарантируется, что методы Remove и MoveToFront вызываются от существующих в списке элементов." - пропускаем
+	if i == l.frontItem {
+		return
+	}
+	l.Remove(i)
+	l.PushFront(i.Value)
 }
 
 func NewList() List {
